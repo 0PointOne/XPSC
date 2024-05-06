ce3cf8cce8d2e78a3b6cdc64222c2310247710b6
0PointOne <zishanabder@gmail.com>
2024-05-06 23:52:55 +0600

    Week 7 Day 6 & 7

---
+++ Week 7/Day 7/E_Binary_Inversions.cpp
@@ -0,0 +1,106 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define int long long
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef unsigned long long ull;
+typedef long double lld;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << nline;
+#else
+#define debug(x)
+#endif
+
+void _print(int t) {cerr << t;}
+void _print(bool t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+
+void solve(){
+
+    int n;  cin >> n;
+    vector<int> v(n);
+    for(int i = 0; i < n; i++)  cin >> v[i];
+
+    int mx = 0;
+    int cnt_z = 0, cnt_z2 = 0;
+
+    for(int i = n-1; i >= 0; i--){
+        if(v[i] == 0) cnt_z++;
+        else mx += cnt_z;
+    }
+    debug(mx)
+
+    vector<int> c = v;
+    for(int i = 0; i < n; i++){
+        if(v[i] == 0){
+            v[i] = 1;
+            break;
+        }
+    }
+    for(int i = n-1; i >= 0; i--){
+        if(c[i] == 1){
+            c[i] = 0;
+            break;
+        }
+    }
+    
+    int mx_cnt = 0, mx_cnt2 = 0;
+    cnt_z = 0;
+
+    for(int i = n-1; i >= 0; i--){
+        if(v[i] == 0) cnt_z++;
+        else mx_cnt += cnt_z;
+
+        if(c[i] == 0) cnt_z2++;
+        else    mx_cnt2 += cnt_z2;
+    }
+
+    debug(mx_cnt)
+    debug(mx_cnt2)
+    mx = max({mx, mx_cnt, mx_cnt2});
+
+    cout << mx << nline;
+}
+
+signed main() {
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+#endif
+
+    fastio();
+    int t = 1;   	
+    cin >> t;
+    while(t--){     solve(); }
+    return 0;
+}
