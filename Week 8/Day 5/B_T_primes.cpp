0cdb0b43522d0288d2aebe4531680192bad404b7
0PointOne <zishanabder@gmail.com>
2024-05-16 19:22:42 +0600

    Week 8 Day 5

---
+++ Week 8/Day 5/B_T_primes.cpp
@@ -0,0 +1,105 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define int long long
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef unsigned long long ull;
+typedef long double lld;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << nline;
+#else
+#define debug(x)
+#endif
+
+void _print(int t) {cerr << t;}
+void _print(bool t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+int findroot(__int64 n){
+    int sqr = sqrt(n);
+    int result = (int)(sqr);
+    return result;
+}
+
+bool isPri(__int64 n){
+
+    if(n <= 1) return false;
+
+    for(__int64 i = 2; i * i <= n; i++){
+        if(n % i == 0) return false;
+    }
+    return true;
+}
+
+
+void solve(){
+
+    __int64 n;  cin >> n;
+
+    int k = 1e6;
+    vector<bool> pr(k + 1, true);
+    pr[1] = false;
+    for(int i = 2; i * i <= k; i++){
+        if(pr[i]){
+            for(int j = i * i; j <= k; j += i)   pr[j] = false;
+        }
+    }
+    debug(pr)
+
+    for(__int64 i = 1; i <= n; i++){
+        __int64 num;  cin >> num;
+
+        int r = findroot(num);
+        debug(r)
+
+        if(r * r == num && pr[r]){
+            cout << "YES" << nline;
+        }
+        else if(r * r != num || !pr[r]){
+            cout << "NO" << nline;
+        }
+    }
+    
+}
+
+signed main() {
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+#endif
+
+    fastio();
+    int t = 1;   	
+    // cin >> t;
+    while(t--){     solve(); }
+    return 0;
+}
