f22ccb581dccefc6c2edccb329ee408fc32b4d94
0PointOne <zishanabder@gmail.com>
2024-09-12 12:34:47 +0600

    update

--- Week 16/Day 5/A_King_Escape.cpp
+++ Week 16/Day 5/A_King_Escape.cpp
@@ -10,47 +10,6 @@ void solve(){
     int kx, ky; cin >> kx >> ky;
     int cx, cy; cin >> cx >> cy;
 
-    if(qx == kx || qx == cx || qy == ky || qy == cy){
-        cout << "NO";
-        return;
-    }
-
-    int tx = qx, ty = qy;
-    while(tx <= n && ty <= n){
-        if(cx == tx && cy == ty){
-            cout << "NO";
-            return;
-        }
-        tx++, ty++;
-    }
-    tx = qx, ty = qy;
-    while(tx >= 0 && ty >= 0){
-        if(cx == tx && cy == ty){
-            cout << "NO";
-            return;
-        }
-        tx--, ty--;
-    }
-
-    tx = qx, ty = qy;
-    while(tx >= 0 && ty <= n){
-        if(tx == cx && ty == cy){
-            cout << "NO";
-            return;
-        }
-        tx--;
-        ty++;
-    }
-    tx = qx, ty = qy;
-    while(tx <= n && ty >= 0){
-        if(tx == cx && ty == cy){
-            cout << "NO";
-            return;
-        }
-        tx++;
-        ty--;
-    }
-
     if(cx < qx && cy < qy && kx < qx && ky < qy) cout << "YES";
     else if(cx > qx && cy > qy && kx > qx && ky > qy) cout << "YES";
     else if(cx > qx && cy < qy && kx > qx && ky < qy) cout << "YES";
